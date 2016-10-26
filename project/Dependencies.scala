2a7945217581ad3af2518f43c35f37fd50a7c90e
tkqubo <tk.qubo@gmail.com>
2016-10-26 22:20:08 +0900

    chore: update modules

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val akka    = "2.4.10"
-    val specs2  = "3.8.5"
-    val jsoup   = "1.9.2"
+    val akka    = "2.4.11"
+    val specs2  = "3.8.5.1"
+    val jsoup   = "1.10.1"
     val mockScheduler: String = "0.4.0"
   }
 
@@ -16,8 +16,8 @@ object Dependencies {
     "com.typesafe.akka" %%  "akka-http-experimental"            % Versions.akka,
     "com.typesafe.akka" %%  "akka-http-testkit"                 % Versions.akka % Test,
     "com.typesafe.akka" %%  "akka-http-spray-json-experimental" % Versions.akka,
-    "org.jsoup"         %   "jsoup"                             % Versions.jsoup,
     "com.miguno.akka"   %%  "akka-mock-scheduler"               % Versions.mockScheduler % Test,
+    "org.jsoup"         %   "jsoup"                             % Versions.jsoup,
     "org.specs2"        %%  "specs2-core"                       % Versions.specs2 % Test,
     "org.specs2"        %%  "specs2-matcher"                    % Versions.specs2 % Test,
     "org.specs2"        %%  "specs2-matcher-extra"              % Versions.specs2 % Test,
